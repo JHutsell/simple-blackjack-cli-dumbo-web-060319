a954c9d7c96ee9b4247e651b084d3aa7f566f0dd
Justin Hutsell <hutselljustin@gmail.com>
2019-05-29 19:50:41 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,16 +32,29 @@ end
 def initial_round
   # code #initial_round here
   card_total = 0 
-  2.times 
-  
+  2.times { card_total += deal_card } 
+  display_card_total(card_total)
+  card_total
 end
 
-def hit?
+def hit?(card_total)
   # code hit? here
+  prompt_user
+  input = get_user_input
+  if input == "h"
+    card_total += deal_card
+  elsif input == "s"
+    card_total = card_total 
+  else
+    invalid_command
+  end 
+  card_total
 end
 
 def invalid_command
   # code invalid_command here
+  puts "Please enter a valid command."
+  prompt_user
 end
 
 #####################################################
